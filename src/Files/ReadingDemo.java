5e50680c54abe6d7efd32021c6ff11897a917437
saivardhangone <saivardhangone@gmail.com>
2024-06-27 21:23:13 +0530

    reading demo file updated

--- src/Files/ReadingDemo.java
+++ src/Files/ReadingDemo.java
@@ -1,7 +1,13 @@
 package Files;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+
 public class ReadingDemo {
     public static void main(String[] args) {
-
+        List lst =new ArrayList<Integer>();
+        List<String> studentNames=new LinkedList<>();
     }
 }
